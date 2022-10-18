921f9e74e7af36ac07d9400a7f4dbce5cb41390f
thebrandonhoward <thebrandonhoward@outlook.com>
2022-10-18 10:41:58 -0500

    Update setupTests.ts

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -2,4 +2,7 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
+//eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJwbGF0Zm9ybS1zZXJ2aWNlcyIsImlhdCI6MTY2NjEwNjc0MCwiZXhwIjoxNjk3NjQyNzQwLCJhdWQiOiJjYXBpdGFsY29ubmVjdC5jb20iLCJzdWIiOiJ1bnN1YnNjcmliZSIsImlkIjoiSm9obm55IiwidHlwZSI6Ik15X2NvbXBhbnlfbm90aWZpY2F0aW9uX3R5cGUiLCJlbWFpbCI6ImUqKioqKkBlKioqLmNvbSIsIlJvbGUiOiJ1bnN1YnNjcmliZSJ9.6gCOwgJrlMcJVUw8Pu2LFcxiFlrMMhrApr9ULNfVT8g
+// 7f62b650-2ac5-4819-b889-9628c0ec7fc8
+// 9f1b9ad0-38f9-4e8d-be25-51777e597558
 import '@testing-library/jest-dom';
