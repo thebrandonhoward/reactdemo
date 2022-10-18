45172ca74d1a52aed309a610d6e1a4d9c9f80c91
thebrandonhoward <thebrandonhoward@outlook.com>
2022-10-18 08:25:03 -0500

    Adds alternative use state approaches

--- src/components/NewExpense/NewExpenseForm.tsx
+++ src/components/NewExpense/NewExpenseForm.tsx
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './NewExpenseForm.css';
 
 const NewExpenseForm = (props:any) => {
+    // const [enteredTitle, setEnteredTitle] = useState('');
+    // const [enteredAmount, setEnteredAmount] = useState('');
+    // const [enteredDate, setEnteredDate] = useState('');
+
+    const [userInput, setUserInput] = useState({
+        enteredTitle: '',
+        enteredAmount: '',
+        enteredDate: ''
+    });
     const onTitleChangeHandler = (event:any) => {
      console.warn(event.target.value);
+     // setEnteredTitle(event.target.value);
+        setUserInput({
+            ...userInput,
+            enteredTitle: event.target.value,
+        })
+     console.log(userInput.enteredTitle);
+    }
+    const onAmountChangeHandler = (event:any) => {
+        console.warn(event.target.value);
+        // setEnteredAmount(event.target.value);
+        setUserInput({
+            ...userInput,
+            enteredAmount: event.target.value,
+        })
+        console.log(userInput.enteredAmount);
+    }
+    const onDateChangeHandler = (event:any) => {
+        console.warn(event.target.value);
+        // setEnteredDate(event.target.value);
+        setUserInput({
+            ...userInput,
+            enteredDate: event.target.value,
+        })
+        console.log(userInput.enteredDate);
     }
 
     return(
@@ -15,11 +48,11 @@ const NewExpenseForm = (props:any) => {
                 </div>
                 <div>
                     <label>Amount</label>
-                    <input type='number' min='0.01' step='0.01'/>
+                    <input type='number' min='0.01' step='0.01' onChange={onAmountChangeHandler}/>
                 </div>
                 <div>
                     <label>Date</label>
-                    <input type='date' min='2021-01-01' max='2022-12-31'/>
+                    <input type='date' min='2021-01-01' max='2022-12-31' onChange={onDateChangeHandler}/>
                 </div>
             </div>
             <div>
